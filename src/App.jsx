5bf2fbc9c7f5639c91e8aa8929f170393317980d
Victro Cruz <vcruz728@gmail.com>
2025-07-23 11:06:06 -0600

    Testing deployment

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,15 @@ function App() {
   return (
     <>
     <main className="p-8">
-      <h1 className="text-4xl font-bold mb-6">My Portfolio</h1>
+      <h1 className="text-4xl font-bold mb-6">My Portfolio Victor Cruz</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         {projects.map((proj, i) => (
           <ProjectCard key={i} {...proj} />
         ))}
       </div>
     </main>
-  );
-}
-
     </>
-  )
+  );
 }
 
 export default App
